dc696b72224351d8237fb81f4b070ca5df791422
Tsvetohek <poshivkin777@gmail.com>
2024-12-12 22:42:39 +0300

    Create lesson_1.py

---
+++ lesson_1.py
@@ -0,0 +1,22 @@
+#Задание 1
+#p=3.14
+#r= int(input())
+#C=2*p*r
+#print("Длинна окружности",C)
+#S=p*r*r
+#print("Площадь круга",S)
+
+#Задание 2
+#x=10
+#y=55
+#print("До замены: x =", x, ", y =", y)
+#x, y = y, x
+#print("После замены: x =", x, ", y =", y)
+
+#Задание 3
+#import math
+#l=float(input())
+#g=9.81
+#t=2*math.pi*math.sqrt(l/g)
+#print(f'{t:.2f}')
+
