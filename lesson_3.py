3c1c79a6642a9002d61f636b2470675f8c278e09
Tsvetohek <poshivkin777@gmail.com>
2024-12-12 22:51:16 +0300

    Create lesson_3.py

---
+++ lesson_3.py
@@ -0,0 +1,19 @@
+#Задание1
+#n = int(input("Введитете натуральное число"))
+#if n<=100 and n>=0:
+    #range(0,n-1,)
+    #total_sum = sum(i**3 for i in range(1, n + 1))
+    #print(f"Сумма кубов {n}: {total_sum}")
+    #else:
+        #print("Число должно быть в диапазоне от 1 до 100.")
+
+#Задание 2 Урок 3
+#i = 1
+#while i < 10:
+    #j = 1
+    #while j < 10:
+        #print("%4d" % (i * j), end="")
+        #j += 1
+    #print()
+    #i += 1
+
